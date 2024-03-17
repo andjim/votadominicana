87c9d6607ac35c21b9f9b3df4426dd113908ace7
Anderson Jimenez <andjim2598@gmail.com>
2024-03-17 01:15:34 -0400

    [UPD] - add postgres database config setting for dev and connect it with django

--- votadom/votadom/settings.py
+++ votadom/votadom/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'votadom.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'votadom',
+        'USER': 'votadom',
+        'PASSWORD': 'secretodev',
+        'HOST': 'postgresdb', 
+        'PORT': '5432',
     }
 }
 
