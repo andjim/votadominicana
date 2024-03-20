5f5d725db463d1f56b99a1805f17c6bb2db03291
Anderson Jimenez <andjim2598@gmail.com>
2024-03-20 10:22:45 -0400

    [UPD] - se agregan modelos base para el aplicativo  On branch 3-desarrollo-de-modelos

--- votadom/api/admin.py
+++ votadom/api/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Candidatura, PartidoPolitico, Eleccion
+from .models import Candidatura, PartidoPolitico, Eleccion, Provincia, Municipio, Sector, Candidato, Circunscripcion,ColegioElectoral
 
 admin.site.register(Candidatura)
 admin.site.register(PartidoPolitico)
 admin.site.register(Eleccion)
+
+admin.site.register(Provincia)
+admin.site.register(Municipio)
+admin.site.register(Sector)
+admin.site.register(Circunscripcion)
+admin.site.register(ColegioElectoral)
+
+admin.site.register(Candidato)
