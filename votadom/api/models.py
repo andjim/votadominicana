d03dc11aecbfe3a17c3cf725192665c38970c99b
Anderson Jimenez <andjim2598@gmail.com>
2024-03-20 10:22:45 -0400

    [UPD] - se agregan modelos base para el aplicativo  On branch 3-desarrollo-de-modelos

--- votadom/api/models.py
+++ votadom/api/models.py
@@ -1,8 +1,32 @@
 from django.db import models
 
-# Tablas dimensionales
+#Tablas de divicion Territorial
+class Provincia(models.Model):
+    cod = models.CharField("Codigo", max_length=10)
+    descripcion = models.CharField("Descripcion", max_length=30)
+
+class Municipio(models.Model):
+    cod = models.CharField("Codigo", max_length=10)
+    descripcion = models.CharField("Descripcion", max_length=30)
+    id_provincia = models.ForeignKey(Provincia, on_delete=models.RESTRICT)
+
+class Sector(models.Model):
+    cod = models.CharField("Codigo", max_length=10)
+    descripcion = models.CharField("Descripcion", max_length=30)
+    id_provincia = models.ForeignKey(Municipio, on_delete=models.RESTRICT)
+
+# Tablas de negocio
+class Circunscripcion(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class ColegioElectoral(models.Model):
+    cod = models.CharField(max_length=10)
+    id_circunscripcion = models.ForeignKey(Circunscripcion, on_delete=models.RESTRICT)
+    id_sector = models.ForeignKey(Sector, on_delete=models.RESTRICT)
+
 class Candidatura(models.Model):
     descripcion = models.CharField(max_length=10)
+    es_dependiente = models.BooleanField(default=False)
 
 class PartidoPolitico(models.Model):
     nombre = models.CharField(max_length=60)
@@ -11,11 +35,12 @@ class PartidoPolitico(models.Model):
     color_secundario = models.CharField(max_length=6)
     logo = models.ImageField(upload_to="partidos")
 
-# 
+# Dependiente de eleccion 
 class Eleccion(models.Model):
     descripcion = models.CharField(max_length=60)
     fecha_apertura = models.DateTimeField()
     fecha_cierre = models.DateTimeField()
+    estado = models.CharField(max_length=10,default="draft")
     
 class Votante(models.Model):
     cedula = models.CharField(max_length=11)
@@ -24,3 +49,9 @@ class Votante(models.Model):
     id_eleccion = models.ForeignKey(Eleccion, on_delete=models.CASCADE)
     cod_verificador = models.CharField(max_length=255)
 
+class Candidato(models.Model):
+    id_partido = models.ForeignKey(PartidoPolitico, on_delete=models.CASCADE)
+    id_candidatura = models.ForeignKey(Candidatura, on_delete=models.CASCADE)
+    id_eleccion = models.ForeignKey(Eleccion, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=60)
+    foto = models.ImageField(upload_to="candidatos")
