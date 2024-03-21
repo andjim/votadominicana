e695f4538251fb6071352758e3f4cd1d33155121
Anderson Jimenez <andjim2598@gmail.com>
2024-03-20 23:39:33 -0400

    [UPD] - add allowed host to the app

--- votadom/votadom/settings.py
+++ votadom/votadom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u@8_s%hgu1)x=bkr-qr!_wcbhxkn7^5p=#@=d3%t2u=zmk%6by
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["10.0.0.7","localhost"]
 
 
 # Application definition
